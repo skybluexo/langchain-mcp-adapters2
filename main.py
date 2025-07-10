f8611ea79b1d921995514e54d00e445fc227b4a4
Francis Cho <fhcho1@yahoo.com>
2025-07-10 16:57:24 -0700

    Enhance main.py with async functionality and load environment variables
    - Refactored main.py to include async support and load environment variables using dotenv.
    - Updated the main function to run asynchronously.
    - Added StdioServerParameters for the math server.
    - Included isort as a new dependency in pyproject.toml and updated uv.lock accordingly.
    - Added a blank line in weather_server.py for consistency.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["D:/ProjectFinal/AI_study/mcp-crash-course/servers/math_server.py"],
+)
+
+async def main():
     print("Hello from mcp-crash-course!")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
