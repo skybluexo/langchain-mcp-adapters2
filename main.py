f12bdc18efc2a3b5e5518d9dc150548fc0ffab77
Francis Cho <fhcho1@yahoo.com>
2025-07-10 16:23:00 -0700

    Removed .env from tracking

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import asyncio
-from dotenv import load_dotenv
-import os
-load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
-
-
-async def main():
+def main():
     print("Hello from mcp-crash-course!")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
