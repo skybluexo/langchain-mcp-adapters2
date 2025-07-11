c0d9385d64ada254cbe3ab08ab96998a4d5811c6
Francis Cho <fhcho1@yahoo.com>
2025-07-11 09:27:11 -0700

    Implement agent invocation in main.py for math queries
    - Enhanced the main function to initialize a session and load MCP tools.
    - Integrated the creation of a React agent and added functionality to invoke it with a sample math query.
    - Utilized HumanMessage from langchain_core for structured message handling.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
@@ -18,8 +19,16 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            
+            agent = create_react_agent(llm,tools)
 
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
